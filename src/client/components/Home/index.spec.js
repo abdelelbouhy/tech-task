bfe74c50cb9b2962f22a0d58f49de2ff5f3d9f42
abdel elbouhy <abdel.bouhy@lloydsbanking.com>
2020-08-23 15:37:07 +0100

    Test: Add save button unit test

--- src/client/components/Home/index.spec.js
+++ src/client/components/Home/index.spec.js
@@ -3,12 +3,17 @@ import {shallow} from 'enzyme';
 import Home from '.';
 
 describe('Home', () => {
-    describe('Text area', () => {
-        describe('Rendering', () => {
-            it('should render text area correctly', () => {
-                const wrapper = shallow(<Home />)
-                expect(wrapper.find('.snippet')).toHaveLength(1);
-            })
-        })
-    })
-})
+    describe('Rendering', () => {
+        it('should render text area correctly', () => {
+            const wrapper = shallow(<Home />);
+            expect(wrapper.find('.snippet')).toHaveLength(1);
+        });
+
+        it('should render save button correctly', () => {
+            const wrapper = shallow(<Home />);
+            const input = wrapper.find('.save')
+            expect(input).toHaveLength(1);
+            expect(input.prop('value')).toEqual('Save')
+        });
+    });
+});
