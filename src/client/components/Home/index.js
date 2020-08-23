52fc6aec1cc1f0f2682535feb3ceba3cf89b4bfa
abdel elbouhy <abdel.bouhy@lloydsbanking.com>
2020-08-23 15:46:53 +0100

    feat: Add save button to Home component

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -4,4 +4,5 @@ import HomeStyle from './style';
 export default () =>
     <HomeStyle>
         <input type='text' name='snippet' className='snippet' placeholder='Please enter your text here...'/>
+        <input type='submit' className='save' value='Save' />
     </HomeStyle>
--- src/client/components/Home/style.js
+++ src/client/components/Home/style.js
@@ -1,9 +1,18 @@
 import styled from 'styled-components';
 
 export default styled.div`
+ width: 40%;
+ display: flex;
+ flex-direction: row;
+ justify-content: space-between;
+ 
  .snippet {
     width: 300px;
     height: 50px;
     padding-left: 10px;
+ }
+ 
+ .save {
+    width: 200px
  }
 `;
