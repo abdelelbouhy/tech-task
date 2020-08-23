89e247d7a9976d50d7b5ff775f7877f0ad02d768
abdel elbouhy <abdel.bouhy@lloydsbanking.com>
2020-08-23 15:34:59 +0100

    feat: Add text field to home component

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -1,3 +1,7 @@
 import React from 'react';
+import HomeStyle from './style';
 
-export default () => null
+export default () =>
+    <HomeStyle>
+        <input type='text' name='snippet' className='snippet' placeholder='Please enter your text here...'/>
+    </HomeStyle>
---
+++ src/client/components/Home/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default styled.div`
+ .snippet {
+    width: 300px;
+    height: 50px;
+    padding-left: 10px;
+ }
+`;
